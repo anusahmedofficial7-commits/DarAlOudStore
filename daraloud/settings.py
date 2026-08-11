31b38ae0ad34f2a0c1e7c7434dac71e072835fab
anusahmedofficial7-commits <anusahmedofficial7@gmail.com>
2026-08-12 02:30:58 +0500

    Configure Cloudinary and WhatsApp

--- daraloud/settings.py
+++ daraloud/settings.py
@@ -238,3 +238,14 @@ MEDIA_URL = "/media/"
 # =====================================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# ==============================
+# CLOUDINARY CONFIGURATION
+# ==============================
+
+import cloudinary
+
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+)
